c00139973c3b19d2a9c91a197cb786f06c9b9eb0
Merwin Dale <merwindale.domingo@gmail.com>
2016-04-03 18:59:11 -0700

    Update facebook

--- application/controllers/facebook.php
+++ application/controllers/facebook.php
@@ -1,10 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+
+
 class Facebook extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-
+        //$this->load->library('session');
         $this->load->helper('url');         // get base_url
     }
 
@@ -32,80 +34,96 @@ class Facebook extends CI_Controller {
 
         require_once(APPPATH.'libraries/Facebook/autoload.php');
 
+         $fb = new Facebook\Facebook([
+            'app_id' => FACEBOOK_APP_ID,
+            'app_secret' => FACEBOOK_APP_SECRET,
+            'default_graph_version' => 'v2.2',
+        ]);
+
+
+        $helper = $fb->getRedirectLoginHelper();
+
+        try {
+            $accessToken = $helper->getAccessToken();
+        } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+        exit;
+        } catch(Facebook\Exceptions\FacebookSDKException $e) {
+          // When validation fails or other local issues
+         echo 'Facebook SDK returned an error: ' . $e->getMessage();
+             exit;
+        }
+
+if (! isset($accessToken)) {
+  if ($helper->getError()) {
+    header('HTTP/1.0 401 Unauthorized');
+    echo "Error: " . $helper->getError() . "\n";
+    echo "Error Code: " . $helper->getErrorCode() . "\n";
+    echo "Error Reason: " . $helper->getErrorReason() . "\n";
+    echo "Error Description: " . $helper->getErrorDescription() . "\n";
+  } else {
+    header('HTTP/1.0 400 Bad Request');
+    echo 'Bad request';
+  }
+  exit;
+}
+
+// Logged in
+echo '<h3>Access Token</h3>';
+var_dump($accessToken->getValue());
+
+// The OAuth 2.0 client handler helps us manage access tokens
+$oAuth2Client = $fb->getOAuth2Client();
+
+// Get the access token metadata from /debug_token
+$tokenMetadata = $oAuth2Client->debugToken($accessToken);
+echo '<h3>Metadata</h3>';
+var_dump($tokenMetadata);
+
+// Validation (these will throw FacebookSDKException's when they fail)
+$tokenMetadata->validateAppId(FACEBOOK_APP_ID); // Replace {app-id} with your app id
+// If you know the user ID this access token belongs to, you can validate it here
+//$tokenMetadata->validateUserId('123');
+$tokenMetadata->validateExpiration();
+
+    if (! $accessToken->isLongLived()) {
+      // Exchanges a short-lived access token for a long-lived one
+      try {
+        $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+      } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+        exit;
+      }
+
+      echo '<h3>Long-lived</h3>';
+      var_dump($accessToken->getValue());
+    }
+    $_SESSION['fb_access_token'] = (string) $accessToken;
+    // redirect page
+    }
+
+
+    public function getData() {
+        session_start();
+
+        require_once(APPPATH.'libraries/Facebook/autoload.php');
         $fb = new Facebook\Facebook([
             'app_id' => FACEBOOK_APP_ID,
             'app_secret' => FACEBOOK_APP_SECRET,
             'default_graph_version' => 'v2.2',
         ]);
 
-        $helper = $fb->getRedirectLoginHelper();
+        $accessToken = $_SESSION['fb_access_token'];
 
-        try {
-            if( isset($_SESSION['localhost_app_token']) ) {
-                $accessToken = $_SESSION['localhost_app_token'];
-            } else {
-                $accessToken = $helper->getAccessToken();
-            }
-        } catch(Facebook\Exceptions\FacebookResponseException $e) {
-            // When Graph returns an error
-            echo 'Graph returned an error: ' . $e->getMessage();
-            exit;
-        } catch(Facebook\Exceptions\FacebookSDKException $e) {
-            // When validation fails or other local issues
-            echo 'Facebook SDK returned an error: ' . $e->getMessage();
-            exit;
-        }
+        $response = $fb->get('/me?fields=id,name,email,first_name,last_name', $accessToken);
 
-        if( isset($accessToken)) {
-            if( isset( $_SESSION['localhost_app_token'])) {
-                $fb->setDefaultAccessToken($_SESSION['localhost_app_token']);
-            } else {
-                // getting short-lived access token
-                $_SESSION['localhost_app_token'] = (string) $accessToken;
-
-                // OAuth 2.0 client handler
-                $oAuth2Client = $fb->getOAuth2Client();
-
-                // Exchanges a short-lived access token for a long-lived one
-                $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['localhost_app_token']);
-
-                $_SESSION['localhost_app_token'] = (string) $longLivedAccessToken;
-
-                // setting default access token to be used in script
-                $fb->setDefaultAccessToken($_SESSION['localhost_app_token']);
-            }
-
-            try {
-
-            } catch(Facebook\Exceptions\FacebookResponseException $e) {
-                // When Graph returns an error
-                echo 'Graph returned an error: ' . $e->getMessage();
-                session_destroy();
-                // redirecting user back to app login page
-                header("Location: ./");
-                exit;
-            } catch(Facebook\Exceptions\FacebookSDKException $e) {
-                // When validation fails or other local issues
-                echo 'Facebook SDK returned an error: ' . $e->getMessage();
-                exit;
-            }
-
-            /*
-             * GET BASIC INFO
-             */
-            $response = $fb->get('/me?fields=id,name,email,first_name,last_name', $accessToken);
-
-
-            $me = $response->getGraphUser();
-            echo "<img src='https://graph.facebook.com/".$me->getProperty('id')."/picture?type=large'><br/>";
-            echo "Full Name: ".$me->getProperty('name')."<br>";
-            echo "First Name: ".$me->getProperty('first_name')."<br>";
-            echo "Last Name: ".$me->getProperty('last_name')."<br>";
-            echo "Email: ".$me->getProperty('email')."<br>";
-            echo "Facebook .ID: <a href='https://www.facebook.com/".$me->getProperty('id')."' target='_blank'>".$me->getProperty('id')."</a>";
-
-        } else {
-            header('Location: http://zairus.com/work/facebook_auth/');
-        }
+        $me = $response->getGraphUser();
+        echo "<img src='https://graph.facebook.com/".$me->getProperty('id')."/picture?type=large'><br/>";
+        echo "Full Name: ".$me->getProperty('name')."<br>";
+        echo "First Name: ".$me->getProperty('first_name')."<br>";
+        echo "Last Name: ".$me->getProperty('last_name')."<br>";
+        echo "Email: ".$me->getProperty('email')."<br>";
+   
     }
 }
